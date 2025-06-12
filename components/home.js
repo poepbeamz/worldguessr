e31c319669d77c3899ceadb2ad6c963b097853d7
Gautam Anand <gautamgxtv@gmail.com>
2025-06-12 13:40:42 -0500

    google down

--- components/home.js
+++ components/home.js
@@ -2187,9 +2187,9 @@ if(inCrazyGames) {
               <>
             <h1 className="home__title">WorldGuessr</h1>
 
-              {/* { height > 650 && width > 1000 && (
+              { height > 650 && width > 1000 && (
             <HomeNotice text={text("maintenanceText1", {date: getMaintenanceDate(), time: getTimeString()})} shown={true} />
-              )} */}
+              )}
             </>
             )}
 
